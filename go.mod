b924c1b4abe7e16919248ef28a1193a1ddff5a23
Solakhuddin <Sholahuddin.ihsan@gmail.com>
2024-12-04 20:43:19 +0700

    Update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/Solakhuddin/go-say-hello
+// module github.com/Solakhuddin/go-say-hello
+// major upgrade
+module github.com/Solakhuddin/go-say-hello/v2
 
 go 1.23.3
--- sayHello.go
+++ sayHello.go
@@ -1,5 +1,24 @@
 package golangsayhello
 
-func SayHello() string {
-	return "Hello World"
+// func SayHello() string {
+// 	return "Hello World"
+// }
+
+// major upgrade
+func SayHello(nama string) string {
+	return "Hello World" + nama
 }
+
+/*
+personal notes fot git 
+
+git init
+git add. or git add file_name
+git commit -m "Pesan commit pertama"
+git remote add origin https://github.com/username/nama-repo.git
+git branch -M main
+git push -u origin main
+
+Gunakan git status untuk memeriksa status perubahan.
+
+*/
